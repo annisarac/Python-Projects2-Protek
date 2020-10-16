b1e4403463ea466c205b88db1f5b7deafb935469
Annisa Rachmawati <71623132+annisarac@users.noreply.github.com>
2020-10-16 18:38:34 +0800

    Create latihan02.py

---
+++ praktikum3/latihan02/latihan02.py
@@ -0,0 +1,14 @@
+#Variable assignment
+jarak = 795
+print('Pak Budi menempuh jarak perjalanan sejauh 795 kilometer')
+
+konsumsiBbm = 12
+print('Mobil Pak Budi mnghabiskan 1 liter bensin untuk jarak 12 kilometer')
+
+#Hitung bensin
+literBensin = jarak / konsumsiBbm
+
+totalBensin = int(literBensin)
+
+#Tampilkan hasil
+print('Pak Budi membutuhkan besin sebanyak :' + str(totalBensin) + 'liter')
